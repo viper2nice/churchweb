395fe0fe250fd6d6ae144966854b9e4b54446ff7
Lascell Brown <lascellbrown21@yahoo.com>
2024-06-25 20:42:24 -0400

    commit

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function navBar() {
-  return <h1> Hi Lascell</h1>;
+// interface NavbarProbs {
+//   children: ReactNode;
+//   onClick: ()=> void;
+
+// }
+export default function Navbar() {
+  return (
+    // <ul className="absolute lest-0 top-0">
+    //   <li className="hover:bg-sky-700">hi</li>
+    //   <li>About</li>
+    //   <li>Member</li>
+    // </ul>
+
+    <div></div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Navbar from "./components/navBar";
 
 export default function Home() {
   return (
